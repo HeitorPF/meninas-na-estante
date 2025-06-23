7bf7182562d111c0dfb71adc03d9a3feff9ac43d
HeitorPF <heitorpf12.hf@gmail.com>
2025-06-23 17:17:35 -0300

    dialog

--- scripts/index.js
+++ scripts/index.js
@@ -1,61 +1,146 @@
+let livros = [
+{
+        "id": 2,
+        "titulo": "A Hora da Estrela",
+        "autora": "Clarice Lispector",
+        "genero": "Romance",
+        "descricao": "A história de Macabéa, uma jovem nordestina que vive no Rio de Janeiro enfrentando as dificuldades da vida urbana.",
+        "biografiaAutora": "Clarice Lispector foi uma escritora e jornalista nascida na Ucrânia e naturalizada brasileira, conhecida por sua escrita introspectiva e inovadora.",
+        "anoPublicacao": 1977,
+        "editora": "Rocco",
+        "imagemUrl": null
+    },
+    {
+        "id": 3,
+        "titulo": "Ponciá Vicêncio",
+        "autora": "Conceição Evaristo",
+        "genero": "Romance",
+        "descricao": "O enredo é centrado na herança identitária do avô da protagonista, estabelecendo um diálogo constante entre passado e presente, imaginação e realidade, lembranças e fatos.",
+        "biografiaAutora": "Conceição Evaristo é uma mulher negra que foi empregada doméstica e conseguiu chegar ao topo da academia.",
+        "anoPublicacao": 2017,
+        "editora": "Pallas",
+        "imagemUrl": "https://m.media-amazon.com/images/I/81EULWTz2PL.jpg"
+    },
+    {
+        "id": 4,
+        "titulo": "O peso do pássaro morto",
+        "autora": "Aline Bei",
+        "genero": "Romance",
+        "descricao": "O Peso do Pássaro Morto fala da trajetória de vida de uma mulher. A gente pensa que a personagem poderia ter tido ao menos um suspiro na vida, porque ela passa por muitas desgraças, mas, ao final, ela alcança uma alegria genuína com a vida que leva",
+        "biografiaAutora": "Aline Bei é uma escritora brasileira nascida em São Paulo, em 9 de outubro de 1987. Formada em Letras pela Pontifícia Universidade Católica de São Paulo (PUC-SP) e em Artes Cênicas pelo Teatro Escola Célia Helena, também possui pós-graduação em Escritas Performáticas pela PUC-Rio.",
+        "anoPublicacao": 2017,
+        "editora": "Nós",
+        "imagemUrl": "https://m.media-amazon.com/images/I/61QCWnAtMJL.jpg"
+    },
+    {
+        "id": 5,
+        "titulo": "O parque das irmãs magníficas",
+        "autora": "Camila Sosa Villada",
+        "genero": "Romance",
+        "descricao": "Neste livro, a escritora argentina Camila Sosa Villada, de 42 anos, conta a história da personagem tia Encarna, uma travesti de 178 anos que, em uma noite, encontra um bebê abandonado e decide adotá-lo.",
+        "biografiaAutora": "Camila Sosa Villada é uma escritora, atriz e dramaturga argentina nascida em 28 de janeiro de 1982, em La Falda, província de Córdoba. Graduou-se em Comunicação Social e Teatro pela Universidade Nacional de Córdoba. Sua carreira artística começou no teatro, com destaque para a peça Carnes tolendas (2009), um retrato cênico autobiográfico sobre sua identidade travesti.",
+        "anoPublicacao": 2021,
+        "editora": "Tusquets",
+        "imagemUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTepau81jpzX41795uUaUGtVScGwP4aQebAAA&s"
+    },
+    {
+        "id": 2,
+        "titulo": "A Hora da Estrela",
+        "autora": "Clarice Lispector",
+        "genero": "Romance",
+        "descricao": "A história de Macabéa, uma jovem nordestina que vive no Rio de Janeiro enfrentando as dificuldades da vida urbana.",
+        "biografiaAutora": "Clarice Lispector foi uma escritora e jornalista nascida na Ucrânia e naturalizada brasileira, conhecida por sua escrita introspectiva e inovadora.",
+        "anoPublicacao": 1977,
+        "editora": "Rocco",
+        "imagemUrl": null
+    },
+    {
+        "id": 3,
+        "titulo": "Ponciá Vicêncio",
+        "autora": "Conceição Evaristo",
+        "genero": "Romance",
+        "descricao": "O enredo é centrado na herança identitária do avô da protagonista, estabelecendo um diálogo constante entre passado e presente, imaginação e realidade, lembranças e fatos.",
+        "biografiaAutora": "Conceição Evaristo é uma mulher negra que foi empregada doméstica e conseguiu chegar ao topo da academia.",
+        "anoPublicacao": 2017,
+        "editora": "Pallas",
+        "imagemUrl": "https://m.media-amazon.com/images/I/81EULWTz2PL.jpg"
+    },
+    {
+        "id": 4,
+        "titulo": "O peso do pássaro morto",
+        "autora": "Aline Bei",
+        "genero": "Romance",
+        "descricao": "O Peso do Pássaro Morto fala da trajetória de vida de uma mulher. A gente pensa que a personagem poderia ter tido ao menos um suspiro na vida, porque ela passa por muitas desgraças, mas, ao final, ela alcança uma alegria genuína com a vida que leva",
+        "biografiaAutora": "Aline Bei é uma escritora brasileira nascida em São Paulo, em 9 de outubro de 1987. Formada em Letras pela Pontifícia Universidade Católica de São Paulo (PUC-SP) e em Artes Cênicas pelo Teatro Escola Célia Helena, também possui pós-graduação em Escritas Performáticas pela PUC-Rio.",
+        "anoPublicacao": 2017,
+        "editora": "Nós",
+        "imagemUrl": "https://m.media-amazon.com/images/I/61QCWnAtMJL.jpg"
+    },
+    {
+        "id": 5,
+        "titulo": "O parque das irmãs magníficas",
+        "autora": "Camila Sosa Villada",
+        "genero": "Romance",
+        "descricao": "Neste livro, a escritora argentina Camila Sosa Villada, de 42 anos, conta a história da personagem tia Encarna, uma travesti de 178 anos que, em uma noite, encontra um bebê abandonado e decide adotá-lo.",
+        "biografiaAutora": "Camila Sosa Villada é uma escritora, atriz e dramaturga argentina nascida em 28 de janeiro de 1982, em La Falda, província de Córdoba. Graduou-se em Comunicação Social e Teatro pela Universidade Nacional de Córdoba. Sua carreira artística começou no teatro, com destaque para a peça Carnes tolendas (2009), um retrato cênico autobiográfico sobre sua identidade travesti.",
+        "anoPublicacao": 2021,
+        "editora": "Tusquets",
+        "imagemUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTepau81jpzX41795uUaUGtVScGwP4aQebAAA&s"
+    },
+    {
+        "id": 2,
+        "titulo": "A Hora da Estrela",
+        "autora": "Clarice Lispector",
+        "genero": "Romance",
+        "descricao": "A história de Macabéa, uma jovem nordestina que vive no Rio de Janeiro enfrentando as dificuldades da vida urbana.",
+        "biografiaAutora": "Clarice Lispector foi uma escritora e jornalista nascida na Ucrânia e naturalizada brasileira, conhecida por sua escrita introspectiva e inovadora.",
+        "anoPublicacao": 1977,
+        "editora": "Rocco",
+        "imagemUrl": null
+    },
+    {
+        "id": 3,
+        "titulo": "Ponciá Vicêncio",
+        "autora": "Conceição Evaristo",
+        "genero": "Romance",
+        "descricao": "O enredo é centrado na herança identitária do avô da protagonista, estabelecendo um diálogo constante entre passado e presente, imaginação e realidade, lembranças e fatos.",
+        "biografiaAutora": "Conceição Evaristo é uma mulher negra que foi empregada doméstica e conseguiu chegar ao topo da academia.",
+        "anoPublicacao": 2017,
+        "editora": "Pallas",
+        "imagemUrl": "https://m.media-amazon.com/images/I/81EULWTz2PL.jpg"
+    },
+    {
+        "id": 4,
+        "titulo": "O peso do pássaro morto",
+        "autora": "Aline Bei",
+        "genero": "Romance",
+        "descricao": "O Peso do Pássaro Morto fala da trajetória de vida de uma mulher. A gente pensa que a personagem poderia ter tido ao menos um suspiro na vida, porque ela passa por muitas desgraças, mas, ao final, ela alcança uma alegria genuína com a vida que leva",
+        "biografiaAutora": "Aline Bei é uma escritora brasileira nascida em São Paulo, em 9 de outubro de 1987. Formada em Letras pela Pontifícia Universidade Católica de São Paulo (PUC-SP) e em Artes Cênicas pelo Teatro Escola Célia Helena, também possui pós-graduação em Escritas Performáticas pela PUC-Rio.",
+        "anoPublicacao": 2017,
+        "editora": "Nós",
+        "imagemUrl": "https://m.media-amazon.com/images/I/61QCWnAtMJL.jpg"
+    },
+    {
+        "id": 5,
+        "titulo": "O parque das irmãs magníficas",
+        "autora": "Camila Sosa Villada",
+        "genero": "Romance",
+        "descricao": "Neste livro, a escritora argentina Camila Sosa Villada, de 42 anos, conta a história da personagem tia Encarna, uma travesti de 178 anos que, em uma noite, encontra um bebê abandonado e decide adotá-lo.",
+        "biografiaAutora": "Camila Sosa Villada é uma escritora, atriz e dramaturga argentina nascida em 28 de janeiro de 1982, em La Falda, província de Córdoba. Graduou-se em Comunicação Social e Teatro pela Universidade Nacional de Córdoba. Sua carreira artística começou no teatro, com destaque para a peça Carnes tolendas (2009), um retrato cênico autobiográfico sobre sua identidade travesti.",
+        "anoPublicacao": 2021,
+        "editora": "Tusquets",
+        "imagemUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTepau81jpzX41795uUaUGtVScGwP4aQebAAA&s"
+    }
+]
+
 window.addEventListener('DOMContentLoaded', () => {
-  fetch('http://localhost:8080/livros')
-    .then(response => response.json())
-    .then(data => {
-      const container = document.querySelector('.card-list');
-      if (!container) {
-        console.error('Elemento .card-list não encontrado');
-        return;
-      }
-
-      container.innerHTML = '';
-
-      const livrosComImagem = data.filter(item => item.imagemUrl);
-
-      livrosComImagem.forEach(item => {
-        const slide = document.createElement('div');
-        slide.className = 'card-item swiper-slide';
-
-        slide.innerHTML = `
-          <a class="card-link" href="#">
-            <img class="card-image" src="${item.imagemUrl}" alt="${item.titulo}" />
-            <h3 class="titulo-livro">${item.titulo}</h3>
-          </a>
-        `;
-
-        container.appendChild(slide);
-      });
-
-      console.log(`Slides carregados: ${livrosComImagem.length}`);
-
-      new Swiper('.container.swiper', {
-        loop: true,
-        spaceBetween: 10,
-        grabCursor: true,
-        autoplay: {
-        delay: 3000,
-        disableOnInteraction: false
-        },
-        observer: true,
-        observeParents: true,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true
-        },
-        breakpoints: {
-          0: { slidesPerView: 1.2 },
-          768: { slidesPerView: 2 },
-          930: { slidesPerView: 3 },
-          1300: { slidesPerView: 4 },
-          1500: { slidesPerView: 5 }
-        }
-      });
-    })
-    .catch(error => console.error('Erro ao buscar dados:', error));
+  // fetch('http://localhost:8080/livros')
+  // .then(response => response.json())
+  // .then(data => {
+  //   renderList(data)
+  // })
+  // .catch(error => console.error('Erro ao buscar dados:', error));
+  renderList(livros)
 });
 
 
@@ -64,12 +149,14 @@ document.querySelector('.js-pesquisa-header').addEventListener('keydown', (event
     const valor = document.querySelector('.js-pesquisa-header').value;
     console.log(valor);
     
-    fetch(`http://localhost:8080/livros/filtrar?termo=${valor}`)
-      .then(response => response.json())
-      .then(data => {
-        renderList(data);
-        renderResultados(valor, data.length); // <-- agora mostra a quantidade correta
-      });
+    renderList(livros)
+    renderResultados(valor,livros.length)
+    // fetch(`http://localhost:8080/livros/filtrar?termo=${valor}`)
+    // .then(response => response.json())
+    // .then(data => {
+    //   renderList(data);
+    //   renderResultados(valor, data.length); // <-- agora mostra a quantidade correta
+    // });
 
     document.querySelector('.js-pesquisa-header').value = '';
   }
@@ -77,6 +164,41 @@ document.querySelector('.js-pesquisa-header').addEventListener('keydown', (event
 
 
 
+
+function renderModal(id) {
+  const modal = document.querySelector('.modal')
+  let livroMatched;
+  livros.forEach((livro) => {
+    if(livro.id === id) {
+      livroMatched = livro
+    }
+  })
+  const htmlModal = `
+
+  <div class="dialog-conteudo">
+    <button id="fechar-dialog" class="fechar-dialog" onclick="closeModal()">✖</button>
+    <img id="dialog-imagem" src="${livroMatched.imagemUrl}" alt="Capa do livro" class="dialog-imagem" />
+
+    <div class="dialog-info">
+      <h2 id="dialog-titulo" class="dialog-titulo">${livroMatched.titulo}</h2>
+      <p><strong>Autora:</strong> <span id="dialog-autora">${livroMatched.autora}</span></p>
+      <p><strong>Gênero:</strong> <span id="dialog-genero">${livroMatched.genero}</span></p>
+      <p><strong>Ano de Publicação:</strong> <span id="dialog-ano">${livroMatched.anoPublicacao}</span></p>
+      <p><strong>Descrição:</strong> <span id="dialog-descricao">${livroMatched.descricao}</span></p>
+      <p><strong>Biografia da Autora:</strong> <span id="dialog-biografia">${livroMatched.biografiaAutora}</span></p>
+    </div>
+  </div>
+  `
+
+  modal.innerHTML = htmlModal
+  modal.showModal()
+
+}
+
+function closeModal() {
+  const modal = document.querySelector('.modal')
+  modal.close()
+}
 
 function renderList(data) {
 
@@ -95,7 +217,7 @@ function renderList(data) {
     slide.className = 'card-item swiper-slide';
 
     slide.innerHTML = `
-      <a class="card-link" href="#">
+      <a class="card-link" data-id="${item.id}">
         <img class="card-image" src="${item.imagemUrl}" alt="${item.titulo}" />
         <h3 class="titulo-livro">${item.titulo}</h3>
       </a>
@@ -133,8 +255,8 @@ function renderList(data) {
     }
   });
 
-}
 
+}
 function renderResultados(busca, quantidade) {
   if(busca) {
     document.querySelector('.resultado-pesquisa').innerHTML = `
@@ -145,9 +267,18 @@ function renderResultados(busca, quantidade) {
         </div>
     `
     document.querySelector('.subtitle').innerHTML = `Resultados(${quantidade})`
+
+    
   }
   else {
     document.querySelector('.subtitle').innerHTML = `Nosso acervo`
     document.querySelector('.resultado-pesquisa').innerHTML = ''
   }
 }
+
+
+
+  document.querySelector('.subtitle').addEventListener('click', () => {
+
+    renderModal(4)
+  })
